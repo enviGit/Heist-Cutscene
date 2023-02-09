cc18bef0bf2f9c46c6dd46ce389c5dfa8661b205
Paweł Trojański <80624809+enviGit@users.noreply.github.com>
2023-02-09 22:11:21 +0100

    SWAT, driving animations, robbers toggling added
    TODO
    -Poprawić animację lootowania Klauna
    -Poprawić animację wychodzenia Pandy
    -Dopasować animacje pracowników do animacji rabusiów
    -Dodać animację pisania na klawiaturze dla pracowników banku oraz odblask światła od komputera
    -Animacja do poruszania się pojazdów policyjnych oraz zmiany świateł (niebieskie-czerwone)
    -Dołożyc bronie dla SWAT
    -Wyłączyć pojazdy policyjne i włączyć je za pomocą skryptu (VehiclesToggle.cs)
    
    -Dodanie oraz rozmieszczenie kamer, a także ich płynne przechodzenie na Timelinie
    -Konfiguracja skryptów odpowiedzialnych za czas przełączania stanów aktywności obiektów (zmiana otoczenia sejfu, zniknięcie oraz pojawienie się toreb)
    -Dialogi
    -Efekty specjalne (wystrzał, postrzał, krew)
    -Efekty dźwiękowe
    -Oświetlenie
    -Cinemachine efekty Wirtualnych Kamer (Extensions)
    
    -Film grain dla widoku z kamer (Volume, Add Override)
    -Odbicia w szkle (skanowanie obiektów w pobliżu)

--- Assets/Scripts/RobersToggle.cs
+++ Assets/Scripts/RobersToggle.cs
@@ -4,6 +4,7 @@ public class RobersToggle : MonoBehaviour
     public GameObject clown;
     public GameObject panda;
     public GameObject paperBag;
+    public GameObject driver;
     public float waitTimeSeconds;
     public int waitTimeMilliseconds;
     private float waitTime;
@@ -14,12 +15,25 @@ public class RobersToggle : MonoBehaviour
         panda.SetActive(false);
         paperBag.SetActive(false);
         waitTime = waitTimeSeconds + waitTimeMilliseconds / 1000f;
+        Invoke("TurnOff", waitTime - 0.1f);
         Invoke("TurnOn", waitTime);
+        Invoke("Swap", 57.75f);
+    }
+    void TurnOff()
+    {
+        driver.SetActive(false);
     }
     void TurnOn()
     {
         clown.SetActive(true);
         panda.SetActive(true);
         paperBag.SetActive(true);
+    }
+    void Swap()
+    {
+        clown.SetActive(false);
+        panda.SetActive(false);
+        paperBag.SetActive(false);
+        driver.SetActive(true);
     }
 }
